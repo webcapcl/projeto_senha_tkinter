7e0dd70ac6097da80e3850741c27c57ffa676c62
webcapcl <claudionet.pereira@gmail.com>
2026-05-06 21:44:51 -0300

    tela_senha

---
+++ index.py
@@ -0,0 +1,21 @@
+
+from tkinter import *
+from tkinter import messagebox
+
+cliente=Tk()
+cliente.title('Solicitar Senha')
+
+cliente.geometry('400x300')
+
+cliente.configure(bg="#26e0e0")
+contador = 1
+fila = []
+senha_atual = StringVar
+senha_atual.set('---')
+
+
+
+
+
+
+cliente.mainloop()
